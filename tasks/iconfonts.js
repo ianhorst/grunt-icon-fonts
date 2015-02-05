cf1b1b252935ef6c9c2ee5d29d0cb0cd9a15f227
Ian Horst <ian.horst@gmail.com>
2015-02-05 11:58:40 +0000

    initial setup

---
+++ tasks/iconfonts.js
@@ -0,0 +1,19 @@
+/*
+ * grunt-icon-fonts
+ * https://github.com/ianhorst/grunt-icon-fonts
+ *
+ * Copyright (c) 2015 Ian Horst
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('iconfonts', 'Grunt task imports icon fonts from icomoon.io', function() {
+  });
+
+};
---
+++ test/iconfonts_test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var grunt = require('grunt');
+
+exports.iconfonts = {
+  setUp: function(done) {
+    // setup here if necessary
+    done();
+  },
+  todo: function (test) {
+    test.ok(true);
+    test.done();
+  }
+};
